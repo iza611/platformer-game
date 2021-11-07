ef0cfbee1fd39c43bdc4307b247a1215a19b74ed
Izabella Ożóg (Student) <1838267@brunel.ac.uk>
2021-11-07 02:12:00 +0000

    wizard chasing (but falling & not killing)

---
+++ Assets/wizardController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wizardController : MonoBehaviour
+{
+    Rigidbody2D myRB;
+    Animator myAnim;
+
+    bool facingRight;
+    public float maxSpeed;
+
+    public bool horseInCloseArea = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+        facingRight = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (horseInCloseArea)
+        {
+            myAnim.SetBool("seesTheHorse", horseInCloseArea);
+
+            myRB.velocity = new Vector2(1 * maxSpeed, myRB.velocity.y);
+        }
+
+        if (!horseInCloseArea)
+        {
+            myAnim.SetBool("seesTheHorse", horseInCloseArea);
+
+            myRB.velocity = new Vector2(0 * maxSpeed, myRB.velocity.y);
+        }
+       
+    }
+
+    public void setHorseInCloseArea(bool isHorse)
+    {
+        horseInCloseArea = isHorse;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Horse")
+        {
+            horseInCloseArea = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Horse")
+        {
+            horseInCloseArea = true;
+        }
+    }
+
+}
